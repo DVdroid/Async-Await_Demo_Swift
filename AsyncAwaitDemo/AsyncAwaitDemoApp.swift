8ef75cd3a0bc4dc2ee5350f54a90a163017ebb7a
Vikash Anand <anandin02@gmail.com>
2021-06-13 17:55:45 +0530

    Initial Commit

---
+++ AsyncAwaitDemo/AsyncAwaitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitDemoApp.swift
+//  AsyncAwaitDemo
+//
+//  Created by Vikash Anand on 13/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
